2ca49d83c525a9da3ce53fff0bd05f9721eea21a
Misha Bergal <misha.bergal@gmail.com>
2013-12-25 16:03:38 -0600

    A bit of a cleanup.

---
+++ ButterfliesPuzzle.Tests/RotationTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ButterfliesPuzzle.Tests
+    {
+    [TestFixture]
+    public class RotationTest
+        {
+        [Test]
+        public void Should_correctly_rotate_direction()
+            {
+            var direction = Direction.Up;
+            direction.Rotate(Rotation.Rotation90).Should().Be( Direction.Right );
+            direction.Rotate(Rotation.Rotation90).Rotate(Rotation.Rotation90).Should().Be( Direction.Down );
+            direction.Rotate(Rotation.Rotation90).Rotate(Rotation.Rotation90).Rotate(Rotation.Rotation90).Should().Be( Direction.Left );
+            direction.Rotate(Rotation.Rotation90).Rotate(Rotation.Rotation90).Rotate(Rotation.Rotation90).Rotate(Rotation.Rotation90).Should().Be( Direction.Up );
+            }
+        }
+    }
